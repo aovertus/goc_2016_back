ea109c16c57b5fb92fc93d73710fd00d15fca984
aovertus <al3x_over@hotmail.com>
2016-04-09 15:23:59 +0200

    lines index

--- app/controllers/api/lines_controller.rb
+++ app/controllers/api/lines_controller.rb
@@ -1,5 +1,8 @@
 class Api::LinesController < ApiController
 
+  def index
+    @lines = Line.all
+  end
 
   def show
     @line = Line.find_by(line: params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   namespace :api do
-    resources :lines, only: [:show]
+    resources :lines, only: [:index, :show]
   end
 
   # Example of regular route:
